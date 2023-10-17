bf900b355b16909fdae73d80b6ef1e8d34bfd612
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-10-17 14:59:10 +0200

    books api and some other stuff

--- js/script1.js
+++ js/script1.js
@@ -55,15 +55,47 @@ const sl = (data) => {
     busCon.appendChild(textBusTime);
 };
 
-/* fetch('https://api.openweathermap.org/data/2.5/weather?q=huddinge&units=metric&appid=dbf87de7264865416362ce390de95c52')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=huddinge&units=metric&appid=dbf87de7264865416362ce390de95c52')
     .then(res => res.json())
-    .then(json => {
+    .then(data => {
         showInfo(data);
     })
-const showInfo = (data) => {
-    const con = document.getElementById('väder-con');
-    const text = createElement('p')
+    const showInfo = (data) => {
+    const location = document.getElementById('location');
+    const temp = document.querySelector("#tempDiv div")
+    const weatherIcon = document.querySelector("#tempDiv img")
+    const tempDesc = document.querySelector('#väder-con>div>p')
 
-    text.textContent = data.name;
-    con.appendChild(text);
-} */
+    const textWind = document.getElementById('wind');
+    const textHumidity = document.getElementById('humidity');
+    const textVisibility = document.getElementById('visibility');
+    const textPressure = document.getElementById('pressure');
+
+    location.textContent = data.name + ", " + data.sys.country;
+    temp.textContent = Math.floor(data.main.temp) + "°C";
+    weatherIcon.setAttribute('src','https://openweathermap.org/img/wn/'+ data.weather[0].icon +'@4x.png');
+    tempDesc.textContent = 'Feels like ' + Math.floor(data.main.feels_like) + ' °C ' + data.weather[0].main + '. ' + data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+
+    textWind.textContent = 'Wind: ' + data.wind.speed + ' m/s W';
+    textHumidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
+    textVisibility.textContent = 'Visibility: ' + data.visibility/1000 + ' km';
+    textPressure.textContent = 'Pressure: ' + data.main.pressure + ' hPa';
+}
+
+
+function updateTime() {
+    const currentTime = document.getElementById("currentTime");
+
+    let time = new Date();
+    let sweDays = ['Sön','Mån','Tis','Ons','Tor','Fre','Lör'];
+    let sweMonth = ['Jan','Feb','Mar','Apr','Maj','Jul','Jun','Aug','Sep','Okt','Nov','Dec']
+    let day = sweDays[time.getDay()];
+    let date = time.getDate();
+    let month = sweMonth[time.getMonth()]; 
+    let now = time.toLocaleTimeString();
+
+
+   currentTime.textContent = day + ' ' + date + ' ' + month + ' ' + now;
+}
+updateTime();
+setInterval(updateTime, 1000);
